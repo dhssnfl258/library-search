b6c0520860c2ac757a2b8130b86c42d9ab1efeb3
zv322 <dhssnfl258@naver.com>
2025-05-30 00:04:34 +0900

    init

---
+++ src/test/java/com/annolza/springlibrarysearch/SpringLibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annolza.springlibrarysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibrarySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
